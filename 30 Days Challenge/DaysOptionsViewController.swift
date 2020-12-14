04db3b9502515bde8ec1b0dba93c17e31718765b
shotaro.kawaguchi <josfas88@yahoo.co.jp>
2020-12-14 13:18:12 +0900

    commit

---
+++ 30 Days Challenge/DaysOptionsViewController.swift
@@ -0,0 +1,29 @@
+//
+//  DaysOptionsViewController.swift
+//  30 Days Challenge
+//
+//  Created by ShoTora on 2020/12/13.
+//
+
+import UIKit
+
+class DaysOptionsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ 30 Days Challenge/TypeChallengeViewController.swift
@@ -0,0 +1,74 @@
+//
+//  TypeChallengeViewController.swift
+//  30 Days Challenge
+//
+//  Created by ShoTora on 2020/12/13.
+//
+
+import UIKit
+
+class TypeChallengeViewController: UIViewController, UITextFieldDelegate {
+
+    
+    @IBOutlet weak var challengeTextField: UITextField!
+    @IBOutlet weak var nextButton: UIButton!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        challengeTextField.delegate = self
+        challengeTextField.clearButtonMode = .always
+        challengeTextField.clearButtonMode = .whileEditing
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        challengeTextField.endEditing(true)
+        return true
+    }
+        
+    
+    @IBAction func nextButtonPressed(_ sender: UIButton) {
+        if challengeTextField.text != "" {
+            print("OK")
+        } else {
+            challengeTextField.attributedPlaceholder = NSAttributedString(string: "Type Your Challenge Here", attributes: [NSAttributedString.Key.foregroundColor : UIColor.red])
+        }
+    }
+    
+
+    override func shouldPerformSegue(
+        withIdentifier identifier: String,
+        sender: Any?) -> Bool {
+
+        if identifier == "toConfirmationViewController" {
+            if challengeTextField.text != "" {
+                return true
+            }
+        }
+        return false
+    }
+
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "toConfirmationViewController" {
+            let conVC = segue.destination as! ConfirmationViewController
+            conVC.conText = self.challengeTextField.text!
+            
+        }
+    }
+
+    
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
